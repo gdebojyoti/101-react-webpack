4b19069fa0f2a811bd806bc6c711d8a97349b6f4
Debojyoti Ghosh <gdebojyoti@users.noreply.github.com>
2025-01-06 03:40:36 +0530

    Using the bundled main.js as server response.

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,22 @@
 const express = require('express')
+
+const ServerApp = require('../../dist-server/main.js').default
+
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('I got in')
+  const appHtml = ServerApp()
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <title>React SSR</title>
+      </head>
+      <body>
+        <div id="app">${appHtml}</div>
+      </body>
+    </html>
+  `)
 })
 
 app.listen(3000)
