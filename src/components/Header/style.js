2c28d40e8b8490256809ece381b7a52c85b34bc9
Debojyoti Ghosh <gdebojyoti@users.noreply.github.com>
2025-01-16 18:41:46 +0530

    Misc.

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -9,16 +9,16 @@ export const h1AltStyle = css`
   font-style: italic;
 `
 
-// this is not removed from the CSS file even though it is not being used
-export const unusedStyle = css`
-  color: red;
-  text-decoration: dashed;
-`
-
 export const h1Style = css`
   background-color: brown;
 `
 
 export const RandomDiv = styled.div`
   font-weight: ${({ count }) => count ? 'bold' : 'normal'}
+`
+
+// NOTE: this is not removed from the generated CSS file even though it is not being used anywhere
+export const unusedStyle = css`
+  color: red;
+  text-decoration: dashed;
 `
