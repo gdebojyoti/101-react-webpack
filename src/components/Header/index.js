0374981feb25617f9cea69e1d71930c4706f7f7d
Debojyoti Ghosh <gdebojyoti@users.noreply.github.com>
2025-01-16 18:04:57 +0530

    `cx` to combine styles.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
+import { cx } from '@linaria/core'
 
-import { headerStyle, h1Style, RandomDiv } from './style'
+import { headerStyle, h1Style, h1AltStyle, RandomDiv } from './style'
 
 const Header = () => {
   const [counter, setCounter] = useState(0)
@@ -13,7 +14,7 @@ const Header = () => {
 
   return (
     <header className={headerStyle}>
-      <h1 className={h1Style}>I got in.</h1>
+      <h1 className={cx(h1Style, counter && h1AltStyle)}>I got in.</h1>
       <RandomDiv count={counter}>I have {counter} items.</RandomDiv>
     </header>
   )
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -5,6 +5,15 @@ export const headerStyle = css`
   background-color: brown;
   color: gold;
 `
+export const h1AltStyle = css`
+  font-style: italic;
+`
+
+// this is not removed from the CSS file even though it is not being used
+export const unusedStyle = css`
+  color: red;
+  text-decoration: dashed;
+`
 
 export const h1Style = css`
   background-color: brown;
